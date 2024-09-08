800b984e172b5e47be46a448dd942e62ee0af9f3
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-09-08 12:30:43 -0300

    Initial Commit

---
+++ CollageCanvas/CollageCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollageCanvasApp.swift
+//  CollageCanvas
+//
+//  Created by Rodrigo Cavalcanti on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CollageCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
